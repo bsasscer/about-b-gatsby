16248d63e57241cf0faa1363e6b439e4206e4d99
bsasscer <smithbi.maricela@gmail.com>
2018-06-06 15:55:26 -0700

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Bianca Sasscer | Web Developer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+  ],
 }
