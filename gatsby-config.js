06501d5df8d1aa1143089f7ee82721911b40a1db
bsasscer <smithbi.maricela@gmail.com>
2018-06-06 16:30:16 -0700

    tweak directory for blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: 'Bianca Sasscer | Web Developer',
+    title: 'Bianca Sasscer',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [],
       },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-netlify-cms',
   ],
 }
