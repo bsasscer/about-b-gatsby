9e92a39806cac38c285c2b28efc194c9c2d88664
bsasscer <smithbi.maricela@gmail.com>
2018-06-14 12:41:37 -0700

    tweak files structures, fine tune config

--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Template({
+export default function BlogPostTemplate({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
